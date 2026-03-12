3d5b982265d37d5e942a6ce559e65e879dd2666e
Nam Le <thenamle2005@gmail.com>
2026-03-11 22:21:36 -0700

    Initial Commit

---
+++ Sen/SenApp.swift
@@ -0,0 +1,17 @@
+//
+//  SenApp.swift
+//  Sen
+//
+//  Created by Nam Le on 3/11/26.
+//
+
+import SwiftUI
+
+@main
+struct SenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
